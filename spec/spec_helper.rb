640abdd6d63bd341b17c85970bef528a1a69e85d
Chris Armstrong <chris@opdemand.com>
2014-03-28 11:49:16 -0700

    Set default platform/version for Runner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+# Specify defaults -- these can be overridden
+RSpec.configure do |config|
+  config.platform = 'ubuntu'
+  config.version = '12.04'
+end
+
 ChefSpec::Coverage.start!
